53352201bfcf6398d71286051e1bf27489b1a813
Hiwonder <44456375+Hiwonder@users.noreply.github.com>
2020-05-30 11:42:58 +0800

    Blocks for Hiwonder color recognition module

---
+++ README.md
@@ -0,0 +1,14 @@
+# color_sensor
+ Blocks for Hiwonder color recognition module
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/color_sensor
+(The metadata above is needed for package search.)
+
+```package
+color_sensor=github:Hiwonder/color_sensor
+```
---
+++ main.ts
@@ -0,0 +1,317 @@
+/*
+ color_sensor package
+*/
+//% weight=10 icon="\uf140" color=#2896ff
+namespace color_sensor {
+    export enum colors {
+        //% block="Red"
+        Red = 0x01,
+        //% block="Green"
+        Green = 0x02,
+        //% block="Blue"
+        Blue = 0x03,
+        //% block="Black"
+        Black = 0x04,
+        //% block="White"
+        White = 0x05,
+        //% block="None"
+        None = 0x06
+    }
+
+    const APDS9960_I2C_ADDR = 0x39;
+    const APDS9960_ID_1 = 0xA8;
+    const APDS9960_ID_2 = 0x9C;
+    /* APDS-9960 register addresses */
+    const APDS9960_ENABLE = 0x80;
+    const APDS9960_ATIME = 0x81;
+    const APDS9960_WTIME = 0x83;
+    const APDS9960_AILTL = 0x84;
+    const APDS9960_AILTH = 0x85;
+    const APDS9960_AIHTL = 0x86;
+    const APDS9960_AIHTH = 0x87;
+    const APDS9960_PERS = 0x8C;
+    const APDS9960_CONFIG1 = 0x8D;
+    const APDS9960_PPULSE = 0x8E;
+    const APDS9960_CONTROL = 0x8F;
+    const APDS9960_CONFIG2 = 0x90;
+    const APDS9960_ID = 0x92;
+    const APDS9960_STATUS = 0x93;
+    const APDS9960_CDATAL = 0x94;
+    const APDS9960_CDATAH = 0x95;
+    const APDS9960_RDATAL = 0x96;
+    const APDS9960_RDATAH = 0x97;
+    const APDS9960_GDATAL = 0x98;
+    const APDS9960_GDATAH = 0x99;
+    const APDS9960_BDATAL = 0x9A;
+    const APDS9960_BDATAH = 0x9B;
+    const APDS9960_POFFSET_UR = 0x9D;
+    const APDS9960_POFFSET_DL = 0x9E;
+    const APDS9960_CONFIG3 = 0x9F;
+    const APDS9960_GCONF4 = 0xAB;
+    const APDS9960_AICLEAR = 0xE7;
+
+
+    /* LED Drive values */
+    const LED_DRIVE_100MA = 0;
+
+    /* ALS Gain (AGAIN) values */
+    const AGAIN_4X = 1;
+
+    /* Default values */
+    const DEFAULT_ATIME = 219;    // 103ms
+    const DEFAULT_WTIME = 246;    // 27ms
+    const DEFAULT_PROX_PPULSE = 0x87;    // 16us, 8 pulses
+    const DEFAULT_POFFSET_UR = 0;       // 0 offset
+    const DEFAULT_POFFSET_DL = 0;       // 0 offset      
+    const DEFAULT_CONFIG1 = 0x60;    // No 12x wait (WTIME) factor
+    const DEFAULT_AILT = 0xFFFF;  // Force interrupt for calibration
+    const DEFAULT_AIHT = 0;
+    const DEFAULT_PERS = 0x11;    // 2 consecutive prox or ALS for int.
+    const DEFAULT_CONFIG2 = 0x01;    // No saturation interrupts or LED boost  
+    const DEFAULT_CONFIG3 = 0;       // Enable all photodiodes, no SAI
+    const DEFAULT_LDRIVE = LED_DRIVE_100MA;
+    const DEFAULT_AGAIN = AGAIN_4X;
+
+    const OFF = 0;
+    const POWER = 0;
+    const AMBIENT_LIGHT = 1;
+    const ALL = 7;
+
+    const red_wb = 2130;
+    const green_wb = 3500;
+    const blue_wb = 4620;
+
+
+    function mapRGB(x: number, in_min: number, in_max: number, out_min: number, out_max: number): number {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+
+    function i2cwrite(reg: number, value: number) {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = value;
+        pins.i2cWriteBuffer(APDS9960_I2C_ADDR, buf);
+    }
+
+    function i2cread(reg: number): number {
+        pins.i2cWriteNumber(APDS9960_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt8BE);
+        return val;
+    }
+
+
+    function InitColor(): boolean {
+        let id = i2cread(APDS9960_ID);
+        setMode(ALL, OFF);
+        i2cwrite(APDS9960_ATIME, DEFAULT_ATIME);
+        i2cwrite(APDS9960_WTIME, DEFAULT_WTIME);
+        i2cwrite(APDS9960_PPULSE, DEFAULT_PROX_PPULSE);
+        i2cwrite(APDS9960_POFFSET_UR, DEFAULT_POFFSET_UR);
+        i2cwrite(APDS9960_POFFSET_DL, DEFAULT_POFFSET_DL);
+        i2cwrite(APDS9960_CONFIG1, DEFAULT_CONFIG1);
+        setLEDDrive(DEFAULT_LDRIVE);
+        setAmbientLightGain(DEFAULT_AGAIN);
+        setLightIntLowThreshold(DEFAULT_AILT);
+        setLightIntHighThreshold(DEFAULT_AIHT);
+        i2cwrite(APDS9960_PERS, DEFAULT_PERS);
+        i2cwrite(APDS9960_CONFIG2, DEFAULT_CONFIG2);
+        i2cwrite(APDS9960_CONFIG3, DEFAULT_CONFIG3);
+        return true;
+    }
+
+    function setLEDDrive(drive: number) {
+        let val = i2cread(APDS9960_CONTROL);
+        /* Set bits in register to given value */
+        drive &= 0b00000011;
+        drive = drive << 6;
+        val &= 0b00111111;
+        val |= drive;
+        i2cwrite(APDS9960_CONTROL, val);
+    }
+
+    function setLightIntLowThreshold(threshold: number) {
+        let val_low = threshold & 0x00FF;
+        let val_high = (threshold & 0xFF00) >> 8;
+        i2cwrite(APDS9960_AILTL, val_low);
+        i2cwrite(APDS9960_AILTH, val_high);
+    }
+
+    function setLightIntHighThreshold(threshold: number) {
+        let val_low = threshold & 0x00FF;
+        let val_high = (threshold & 0xFF00) >> 8;
+        i2cwrite(APDS9960_AIHTL, val_low);
+        i2cwrite(APDS9960_AIHTH, val_high);
+    }
+
+
+    function rgb2hue(r: number, g: number, b: number): number {
+        let max = Math.max(r, Math.max(g, b))
+        let min = Math.min(r, Math.min(g, b))
+        let c = max - min;
+        let hue = 0;
+        let segment = 0;
+        let shift = 0;
+        if (c == 0)
+            return 0;
+        if ((r > g) && (r > b)) {
+            segment = (60.0 * (g - b)) / c;
+            if (segment < 0)
+                hue = segment + 360;
+        }
+        else if ((g > b) && (g > r)) {
+            segment = (60.0 * (b - r)) / c;
+            hue = segment + 120;
+        }
+        else if ((b > g) && (b > r)) {
+            segment = (60.0 * (r - g)) / c;
+            hue = segment + 240;
+        }
+        return hue;
+    }
+
+    function setMode(mode: number, enable: number) {
+        let reg_val = getMode();
+        /* Change bit(s) in ENABLE register */
+        enable = enable & 0x01;
+        if (mode >= 0 && mode <= 6) {
+            if (enable > 0) {
+                reg_val |= (1 << mode);
+            }
+            else {
+                //reg_val &= ~(1 << mode);
+                reg_val &= (0xff - (1 << mode));
+            }
+        }
+        else if (mode == ALL) {
+            if (enable > 0) {
+                reg_val = 0x7F;
+            }
+            else {
+                reg_val = 0x00;
+            }
+        }
+        i2cwrite(APDS9960_ENABLE, reg_val);
+    }
+
+    function getMode(): number {
+        let enable_value = i2cread(APDS9960_ENABLE);
+        return enable_value;
+    }
+
+    function enableLightSensor(interrupts: boolean) {
+        setAmbientLightGain(DEFAULT_AGAIN);
+        if (interrupts) {
+            setAmbientLightIntEnable(1);
+        }
+        else {
+            setAmbientLightIntEnable(0);
+        }
+        enablePower();
+        setMode(AMBIENT_LIGHT, 1);
+    }
+
+    function setAmbientLightGain(drive: number) {
+        let val = i2cread(APDS9960_CONTROL);
+        /* Set bits in register to given value */
+        drive &= 0b00000011;
+        val &= 0b11111100;
+        val |= drive;
+        i2cwrite(APDS9960_CONTROL, val);
+    }
+
+    function getAmbientLightGain(): number {
+        let val = i2cread(APDS9960_CONTROL);
+        val &= 0b00000011;
+        return val;
+    }
+
+    function enablePower() {
+        setMode(POWER, 1);
+    }
+
+    function setAmbientLightIntEnable(enable: number) {
+        let val = i2cread(APDS9960_ENABLE);
+        /* Set bits in register to given value */
+        enable &= 0b00000001;
+        enable = enable << 4;
+        val &= 0b11101111;
+        val |= enable;
+        i2cwrite(APDS9960_ENABLE, val);
+    }
+
+    function readAmbientLight(): number {
+        let val = i2cread(APDS9960_CDATAL);
+        let val_byte = i2cread(APDS9960_CDATAH);
+        val = val + val_byte * 256;
+        return val;
+    }
+
+    /**
+     * Initialize the color sensor,please execute at boot time
+     */
+    //% weight=98 blockId=init_colorSensor block="Initialize color sensor port at port 4 (P19:SCL,P20:SDA)"
+    export function init_colorSensor() {
+        InitColor();
+        enableLightSensor(true);
+        control.waitMicros(100);
+    }
+
+    /**
+	 *  Color sensor return the color.
+	 */
+    //% weight=96 blockId=checkCurrentColor block="Current color %color"
+    export function checkCurrentColor(color: colors): boolean {
+        let c = i2cread(APDS9960_CDATAL) + i2cread(APDS9960_CDATAH) * 256;
+        let r = i2cread(APDS9960_RDATAL) + i2cread(APDS9960_RDATAH) * 256;
+        let g = i2cread(APDS9960_GDATAL) + i2cread(APDS9960_GDATAH) * 256;
+        let b = i2cread(APDS9960_BDATAL) + i2cread(APDS9960_BDATAH) * 256;
+
+        // serial.writeNumber(c);
+        // serial.writeLine("->ccc");
+        // serial.writeNumber(r);
+        // serial.writeLine("->red");
+        // serial.writeNumber(g);
+        // serial.writeLine("->green");
+        // serial.writeNumber(b);
+        // serial.writeLine("->blue");
+
+        if (r > red_wb)
+            r = red_wb;
+        if (g > green_wb)
+            g = green_wb;
+        if (b > blue_wb)
+            b = blue_wb;
+
+        r = Math.round(mapRGB(r, 0, red_wb, 0, 255));
+        g = Math.round(mapRGB(g, 0, green_wb, 0, 255));
+        b = Math.round(mapRGB(b, 0, blue_wb, 0, 255));
+        // serial.writeNumber(r);
+        // serial.writeLine("->rred");
+        // serial.writeNumber(g);
+        // serial.writeLine("->ggreen");
+        // serial.writeNumber(b);
+        // serial.writeLine("->bblue");
+         let hsv = rgb2hue(r, g, b);
+        // serial.writeNumber(hsv);
+        // serial.writeLine("->hsv");
+        let t = colors.None;
+        if (c > 2200 && r > 65 && g > 65 && b > 65) {
+            t = colors.White;
+        }
+        else if (c > 800) {
+            if (hsv < 8 || hsv > 350)
+                t = colors.Red;
+            else if (hsv > 60 && hsv < 170) {
+                t = colors.Green;
+            }
+            else if (hsv > 210 && hsv < 230) {
+                t = colors.Blue;
+            }
+        }
+        else if (c > 200 && r > 10 && g > 7 && b > 7 && r < 16.5 && g < 15 && b < 14) {
+            t = colors.Black;
+        }
+        return (color == t);
+    }
+}
